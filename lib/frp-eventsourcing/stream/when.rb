16bf47213d82a597d6ee16a11fbe8ef400be49b5
Zilvinas <zil.kucinskas@gmail.com>
2017-06-05 00:50:38 +0300

    Fix bug and refactor when operator

--- lib/frp-eventsourcing/stream/when.rb
+++ lib/frp-eventsourcing/stream/when.rb
@@ -14,15 +14,15 @@ module FrpEventsourcing
       if event[:event_type] == @event_type
         entity_id_hash = extract_entity_id(event)
 
-        if !@resource_type.where(entity_id_hash).exists?
-          resource = @resource_type.new(event[:data])
-          @block.call(resource)
-          resource.save!
-        else
-          resource = @resource_type.where(entity_id_hash).first
-          @block.call(resource, event)
-          resource.save!
-        end
+        resource =
+          if !@resource_type.where(entity_id_hash).exists?
+            @resource_type.new
+          else
+            @resource_type.where(entity_id_hash).first
+          end
+
+        @block.call(resource, event)
+        resource.save!
       end
 
       occur(event)
